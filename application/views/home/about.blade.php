7648f309496b959f150ca49ed1271a51c0f5df57
Graham [at home] <graham@hungryfishmedia.com>
2013-03-17 22:35:48 -0400

    Tweak bio…courtesy my wife!!

--- application/views/home/about.blade.php
+++ application/views/home/about.blade.php
@@ -4,6 +4,33 @@
 
 <section class="container container-960">
 
+    <h2>About me</h2>
+    <p>
+    Give me any project, and I'll give 110% effort every time in order to make things succeed. My greatest pleasure is seeing things work well and to the satisfaction of the stakeholders. 
+    </p>
+
+    <p>
+    I brand myself as a full stack developer who loves writing OOP code all-day either back-end or front-end. I was also a graphic designer for a number of years, so I've got a firm grounding in UI.
+    </p>
+
+    <p>
+    Learning is a big part of my life, and I spend much of my spare time taking formal classes, or learning new technologies that'll help me rock out with the next big project.
+    </p>
+
+    <h3>
+    Proudest Achievement
+    </h3>
+    <p>
+    VIM...I swear by it now. Shortcuts have never felt so useful.
+    </p>
+
+    <h3>
+    Fun Fact
+    </h3>
+    <p>
+    Currently my wife and I call the great city of Boston home,  but my favorite time of year is flying home to the Canadian prairies to make homemade sausage with my family every Xmas.
+    </p>
+    
     <h2>Where you can find me</h2>
     <ul>
         <li>
